1547a91506a80cfca42f0a41360ba735fc919be0
BHAVYA BANSAL <bhavya.bansal@hpe.com>
2022-12-01 21:36:08 -0500

    reverting error

--- src/Details.js
+++ src/Details.js
@@ -19,8 +19,6 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    throw new Error("Lmao");
-
     const { animal, breed, city, state, description, name, images } =
       this.state;
 
